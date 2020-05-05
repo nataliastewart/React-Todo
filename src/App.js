2dc9f1cb171a1a4b6aa6b1f95c1c746c644292da
Natalia <nataliamirandac@hotmail.com>
2020-05-05 10:28:28 -0400

    Add item and handleSubmit is working - Submitting the new item to the Browser

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import TodoList from "./components/TodoList"
-import TodoForm from "./components/TodoForm"
-
-
+import React from "react";
+import TodoList from "./components/TodoList";
+import TodoForm from "./components/TodoForm";
 
 const array = [
   {
-    task: 'Organize Garage',
+    task: "Organize Garage",
     id: 1528817077286,
-    completed: false
+    completed: false,
   },
   {
-    task: 'Bake Cookies',
+    task: "Bake Cookies",
     id: 1528817084358,
-    completed: false
+    completed: false,
   },
   {
-    task: 'Walk the dogs',
+    task: "Walk the dogs",
     id: 1528817084448,
-    completed: false
-  }
+    completed: false,
+  },
 ];
 
 class App extends React.Component {
@@ -27,37 +25,62 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      todoData: array
-    }
+      todoData: array,
+      item: "",
+    };
   }
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   toggleTodo = (clickedID) => {
-    const newArray = this.state.todoData.map((item => {
+    const newArray = this.state.todoData.map((item) => {
       if (item.id === clickedID) {
         return {
           ...item,
-          completed: !item.completed
-        }
+          completed: !item.completed,
+        };
       } else {
         return item;
       }
-
-    })
-    )
-    this.setState({
-      todoData: newArray
     });
+    this.setState({
+      todoData: newArray,
+    });
+  };
 
-  }
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
 
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.addNewItem(this.state.newItem);
+    console.log("Handle submit clicked");
+  };
+
+  addNewItem = () => {
+    const newItem = {
+      task: this.state.item,
+      id: Date.now(),
+      completed: false,
+    };
+    this.setState({
+      todoData: [...this.state.todoData, newItem],
+    });
+  };
 
   render() {
     return (
       <div className="App">
         <div className="header">
           <h2>Welcome to your Todo App!</h2>
-          <TodoForm />
+          <TodoForm
+            handleChange={this.handleChange}
+            addNewItem={this.addNewItem}
+            handleSubmit={this.handleSubmit}
+            todoData={this.state.todoData}
+          />
         </div>
         <TodoList todoData={this.state.todoData} toggleTodo={this.toggleTodo} />
       </div>
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,14 +1,19 @@
-import React from 'react'
+import React from "react";
 
 export default function TodoForm(props) {
-    return (
-        <div>
-            <form>
-                <input type="text" name="item" value={props.item} placeholder="Add a todo..." />
-                <button>Add Todo</button>
-                <button>Clear Completed</button>
-            </form>
-
-        </div>
-    )
+  return (
+    <div>
+      <form onSubmit={props.handleSubmit}>
+        <input
+          type="text"
+          name="item"
+          value={props.item}
+          placeholder="Add a todo..."
+          onChange={props.handleChange}
+        />
+        <button type="submit">Add Todo</button>
+        <button>Clear Completed</button>
+      </form>
+    </div>
+  );
 }
